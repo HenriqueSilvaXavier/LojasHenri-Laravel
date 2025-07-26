bebfbea76f5e19e6b0575f859a1b7472dadfa6a3
HenriqueSilvaXavier <134643923+HenriqueSilvaXavier@users.noreply.github.com>
2025-07-26 09:47:43 -0300

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="/css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.4.1/dist/tailwind.min.css" rel="stylesheet">
     <link rel="shortcut icon" href="/img/logo.png" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Oxanium&display=swap" rel="stylesheet">
 </head>
 <body>
     <header id="header">
